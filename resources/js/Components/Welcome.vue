f1df045f09a05771045443f6a0bd6e075a6785bf
Coşkun Demirel <coskun.4901@gmail.com>
2022-12-07 10:14:29 +0300

    deneme

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -10,7 +10,7 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
             </div>
 
             <div class="mt-8 text-2xl">
-                Welcome to your Jetstream application!
+                Welcome to your Jetstream application!sdfsfsdsfdsfdfds
             </div>
 
             <div class="mt-6 text-gray-500">
